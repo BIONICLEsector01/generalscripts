2a69b58af1a9cb7213257599aaf457f739be76a8
MtMNC <morris.mukau@gmail.com>
2019-06-22 15:16:41 -0700

    add error pages to update script

--- update.sh
+++ update.sh
@@ -3,6 +3,12 @@
 echo "Updating BIONICLEsector01.com"
 echo "#############################"
 
-echo "Updating splash"
+echo "Updating splash page"
 cd /home/swert/biosector01.com/
 git pull
+
+echo "Updating HTTP error pages"
+cd /home/swert/biosector01.com/errors
+git pull
+
+echo "Done!"
