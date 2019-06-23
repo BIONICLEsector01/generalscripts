12d9aa9199610f1cac4ca7c99c407b87b483b741
MtMNC <morris.mukau@gmail.com>
2019-06-23 13:51:07 -0700

    fix style, improve local change detection

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 items_to_update=(
-	'/home/swert/biosector01.com/'
+	'/home/swert/biosector01.com'
 	'/home/swert/biosector01.com/errors'
 )
 
@@ -18,26 +18,25 @@ failed_updates=()
 # -1: local copy needs to be pushed
 # -2: local copy and upstream have diverged
 # -3: local copy has uncommitted changes
-needs_update() {
-	# if has local changes
-	if [ -n "$(git status -s)" ]; then
+needs_update () {
+	# if files in version control have local changes
+	if [ -n "$(git ls-files -m)" ]; then
 		echo -3
 	fi
 
 	local upstream='@{u}'
-	local local=$(git rev-parse @)
-	local remote=$(git rev-parse "$upstream")
-	local base=$(git merge-base @ "$upstream") # last common ancestor of local and remote
-	local status=$(git status -s)
+	local local_id=$(git rev-parse @)
+	local remote_id=$(git rev-parse "$upstream")
+	local base_id=$(git merge-base @ "$upstream") # last common ancestor of local and remote
 
-	elif [ $local = $remote ]; then # don't need to do anything
-	    echo 0
-	elif [ $local = $base ]; then # need pull
+	if [ $local_id = $remote_id ]; then # don't need to do anything
+		echo 0
+	elif [ $local_id = $base_id ]; then # need pull
 		echo 1
-	elif [ $remote = $base ]; then # need push
+	elif [ $remote_id = $base_id ]; then # need push
 		echo -1
 	else # diverged
-	    echo -2
+		echo -2
 	fi
 }
 
@@ -45,34 +44,33 @@ needs_update() {
 update_item_if_needed () {
 	cd $1
 	git fetch -q
-
 	case $(needs_update $1) in
 		0)
-			echo "$1 is already up-to-date"
+			echo -e "\t$1: already up-to-date"
 			unneeded_updates+="$1"
 			;;
 		1)
-			echo "updating $1 from remote..."
+			echo -e "\t$1: updating from remote..."
 			git pull
 			successful_updates+="$1"
 			;;
 		-1)
-			echo "can't update $1 (local is ahead of remote, need to push)"
+			echo -e "\t$1: can't update (local is ahead of remote, need to push)"
 			failed_updates+="$1: local is ahead of remote"
 			;;
 		-2)
-			echo "can't update $1 (local and remote diverged)"
+			echo -e "\t$1: can't update (local and remote diverged)"
 			failed_updates+="$1: local and remote diverged"
 			;;
 		-3)
-			echo "can't update $1 (local has uncommitted changes)"
+			echo -e "\t$1: can't update (local has uncommitted changes)"
 			failed_updates+="$1: local has uncommitted changes"
 			;;
 	esac
 }
 
 update () {
-	echo '########## Attempting updates from git ##########'
+	echo 'Attempting updates from git...'
 
 	# perform updates
 	for i in "${items_to_update[@]}"
@@ -80,42 +78,39 @@ update () {
 		update_item_if_needed $i
 	done
 
-	echo '#################################################'
-	echo
+	# echo '#################################################'
+	# echo
 
 	echo 'Finished updating!'
 	echo
 
-	echo "#################### Summary ####################"
+	echo "Summary:"
 
 	if ! [ ${#unneeded_updates[@]} -eq 0 ]; then
-		echo "${#unneeded_updates[@]} directories were already up-to-date"
+		echo -e "\t${#unneeded_updates[@]} directories were already up-to-date"
 		for i in "${unneeded_updates[@]}"
 		do
-			echo -e "\t$i"
+			echo -e "\t\t$i"
 		done
 	fi
 
 	# if updates succeeded, print which ones
 	if ! [ ${#succeeded_updates[@]} -eq 0 ]; then
-		echo "${#succeeded_updates[@]} directories successfully updated"
+		echo -e "\t${#succeeded_updates[@]} directories successfully updated"
 		for i in "${succeeded_updates[@]}"
 		do
-			echo -e "\t$i"
+			echo -e "\t\t$i"
 		done
 	fi
 
 	# if updates failed, print which ones and why
 	if ! [ ${#failed_updates[@]} -eq 0 ]; then
-		echo "${#failed_updates[@]} directories failed to update"
+		echo -e "\t${#failed_updates[@]} directories failed to update"
 		for i in "${failed_updates[@]}"
 		do
-			echo -e "\t$i"
+			echo -e "\t\t$i"
 		done
 	fi
-
-	echo '#################################################'
-	echo
 }
 
 update
